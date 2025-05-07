dc696fab49dcefbc98ae0b1612e2961c3903dfd3
RomanOsma <romanosma@gmail.com>
2025-05-07 22:40:23 +0200

    07052025

---
+++ Modulo4/matematicas/calculadora.js
@@ -0,0 +1,19 @@
+function sumar(a, b) {
+  return a + b;
+}
+
+function restar(a, b) {
+  return a - b;
+}
+
+sumar(5,3);
+restar(10,4);
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+function dividir(a, b) {
+  if (b === 0) throw new Error("División por cero");
+  return a / b;
+}
